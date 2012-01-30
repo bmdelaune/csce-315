9cceec103e0757c0b201837278c84bf8205baa37
bmdelaune@gmail.com <bmdelaune@gmail.com@98dcdb63-5ba3-fc96-04c0-1f53e3c06be8>
2012-01-30 02:26:08 +0000

    done.  1 bug but not worth it to fix

--- ind_assignment/user_control.cpp
+++ ind_assignment/user_control.cpp
@@ -11,6 +11,10 @@ user_control.cpp
 
 All functions used in the main() that control user input data manipulation
 *******************************************************************************/
+
+/***********************************
+INCLUDES
+***********************************/
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -18,17 +22,32 @@ All functions used in the main() that control user input data manipulation
 #include "Student.h"
 #include "Course.h"
 
+/***********************************
+DECLARATIONS
+***********************************/
 list<Student*> list_of_students;
 list<Course*> list_of_courses;
 
+/***********************************
+OPERATING FUNCTIONS
+***********************************/
+/*
+straightforward input of a new student by adding to the list.
+*/
 void addStudent(string rest_of_line){
 	istringstream iss(rest_of_line,istringstream::out);
 	int student_id;
 	string first_name, last_name, major;
 	iss>>student_id>>first_name>>last_name>>major;
+	
 	list_of_students.push_back(new Student(student_id, first_name+" "+last_name, major));
 }
 
+/*
+Because the course name extended to the end of the line, a special process 
+was necessary to parse the line until the end.  Each line of this function is
+necessary to get the correct input.  DO NOT DELETE OR CHANGE.
+*/
 void addCourse(string rest_of_line){
 	istringstream iss(rest_of_line,istringstream::out);
 	iss.seekg(0,ios_base::end);
@@ -36,22 +55,30 @@ void addCourse(string rest_of_line){
 	iss.seekg(0,ios_base::beg);
 	int course_id, max_enrollment;
 	iss>>course_id>>max_enrollment;
+	
 	int line_current_position = iss.tellg();
+	//finds length of course name
 	int course_name_length = line_length-line_current_position;
 	char temp_name[course_name_length];
 	iss.seekg(1,ios_base::cur);
 	iss.getline(temp_name,course_name_length);
 	string course_name(temp_name);
+	
 	list_of_courses.push_back(new Course(course_id, max_enrollment, course_name));
 }
 
+/*
+Easiest way to update the Max Enrollment and change both student/course data.
+*/
 void changeCourseMax(string rest_of_line){
 	istringstream iss(rest_of_line,istringstream::out);
 	int temp_course_id, temp_new_enrollment;
 	iss>>temp_course_id>>temp_new_enrollment;
+	
 	for(list<Course*>::iterator it = list_of_courses.begin(); it != list_of_courses.end(); it++){
 		if(temp_course_id == (*it)->returnCourseID()) {
 			(*it)->newMaxEnrollment(temp_new_enrollment);
+			//checks if students need to be moved from the waitlist to the enrolled list
 			while(!(*it)->students_on_waitlist.empty()
 					&& ((*it)->returnMaxEnrollment() > (*it)->students_enrolled.size())) {
 				Student * temp_student_ptr = (*it)->students_on_waitlist.front();
@@ -70,12 +97,17 @@ void changeCourseMax(string rest_of_line){
 	}
 }
 
+/*
+Adds student and Course information.
+*/
 void placeStudentInCourse(string rest_of_line){
 	istringstream iss(rest_of_line,istringstream::out);
 	int temp_course_id, temp_student_id;
 	iss>>temp_course_id>>temp_student_id;
+	
 	Course * temp_course_ptr;
 	Student * temp_student_ptr;
+	
 	for(list<Course*>::iterator it = list_of_courses.begin(); it != list_of_courses.end(); it++){
 		if(temp_course_id == (*it)->returnCourseID()) {
 			temp_course_ptr = (*it);
@@ -86,6 +118,7 @@ void placeStudentInCourse(string rest_of_line){
 			temp_student_ptr = (*it);
 		}
 	}
+	//checks if the student should be enrolled or waitlisted
 	if(temp_course_ptr->returnMaxEnrollment() > temp_course_ptr->students_enrolled.size()){
 		temp_course_ptr->students_enrolled.push_back(temp_student_ptr);
 		temp_student_ptr->courses_enrolled.push_back(temp_course_ptr);
@@ -95,7 +128,10 @@ void placeStudentInCourse(string rest_of_line){
 	}
 }
 
-
+/*
+Must check all possible places the student's information is and remove from
+all lists.
+*/
 void dropStudentFromCourse(string rest_of_line){
 	istringstream iss(rest_of_line,istringstream::out);
 	int temp_course_id, temp_student_id;
@@ -140,28 +176,38 @@ void dropStudentFromCourse(string rest_of_line){
 	}
 }
 
+/*
+finds the student and uses the ostream to output information
+*/
 void printStudentCourses(string rest_of_line){
 	istringstream iss(rest_of_line,istringstream::out);
 	int temp_student_id;
 	iss>>temp_student_id;
+	
 	Student * temp_student_ptr;
 	for(list<Student*>::iterator it = list_of_students.begin(); it != list_of_students.end(); it++){
 		if(temp_student_id == (*it)->returnStudentID()) {
 			temp_student_ptr = (*it);
 		}
 	}
+	
 	cout<<(*temp_student_ptr)<<endl;
 }
 
+/*
+finds the course and uses the ostream to output information
+*/
 void printCourseEnrollment(string rest_of_line){
 	istringstream iss(rest_of_line,istringstream::out);
 	int temp_course_id;
 	iss>>temp_course_id;
+	
 	Course * temp_course_ptr;
 	for(list<Course*>::iterator it = list_of_courses.begin(); it != list_of_courses.end(); it++){
 		if(temp_course_id == (*it)->returnCourseID()) {
 			temp_course_ptr = (*it);
 		}
 	}
+	
 	cout<<(*temp_course_ptr)<<endl;
 }
